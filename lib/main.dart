0aab95247db0a9897387e0a964fc43ad749832d0
Kyara <kyara.altamirano@epn.edu.ec>
2026-06-05 16:48:01 -0500

    google maps

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:url_launcher/url_launcher.dart';
 
 void main() => runApp(const MyApp());
 class MyApp extends StatelessWidget {
@@ -17,52 +18,52 @@ class MyApp extends StatelessWidget {
             children: [
               ImageSection(image: 'images/lake.jpeg'),
               TitleSection(name: 'Oeschinen Lake Campground', location: 'Kandersteg, Switzerland'),
-              ButtonSection(),
+              ButtonSection( mapsUrl: 'https://maps.app.goo.gl/XCzSoDcT1RznCFtL7'),
               TextSection(description: 'Hermoso lago alpino rodeado de montañas y bosques, ideal para realizar caminatas, acampar y disfrutar de impresionantes paisajes naturales.'),
 
               ImageSection(image: 'images/lake2.jpeg'),
               TitleSection(name: 'Paisaje de niebla', location: 'Ecuador'),
-              ButtonSection(),
+              ButtonSection(mapsUrl: 'https://maps.app.goo.gl/NLMhun7oD9wMUhkj8'),
               TextSection(description: 'Un paisaje cubierto por una suave capa de niebla que resalta la belleza natural de los valles y montañas ecuatorianas.'),
 
               ImageSection(image: 'images/lake3.jpeg'),              
               TitleSection(name: 'Volcán Cotopaxi', location: 'Cotopaxi'),
-              ButtonSection(),
+              ButtonSection(mapsUrl: 'https://maps.app.goo.gl/UbE45SZKUac1VJVY7'),
               TextSection(description: 'Un volcán activo que ofrece vistas impresionantes de sus cráteres y la cordillera circundante.'),
 
               ImageSection(image: 'images/lake4.jpeg'),
               TitleSection(name: 'Centro Histórico', location: 'UIO'),
-              ButtonSection(),
+              ButtonSection(mapsUrl: 'https://maps.app.goo.gl/NBx7uoDcTVm1zCAw8'),
               TextSection(description: 'El Centro Histórico de Quito destaca por su arquitectura colonial, iglesias emblemáticas y calles llenas de historia y cultura.'),
 
               ImageSection(image: 'images/lake5.jpeg'),
               TitleSection(name: 'Panecillo', location: 'QUITO'),
-              ButtonSection(),
+              ButtonSection(mapsUrl: 'https://maps.app.goo.gl/s1USTaxRjMVx3n7i8'),
               TextSection(description: 'El Panecillo ofrece una vista panorámica de la ciudad de Quito y alberga la icónica estatua de la Virgen de Quito.El Panecillo ofrece una vista panorámica de la ciudad de Quito y alberga la icónica estatua de la Virgen de Quito.'),
 
               ImageSection(image: 'images/lake6.jpeg'),
               TitleSection(name: 'Mitad del mundo', location: 'QUITO'),
-              ButtonSection(),
+              ButtonSection(mapsUrl: 'https://maps.app.goo.gl/TCJvVwHu6Kw7HXdi8'),
               TextSection(description: 'Monumento turístico ubicado sobre la línea ecuatorial, donde los visitantes pueden conocer más sobre la geografía y cultura del Ecuador.'),
 
               ImageSection(image: 'images/lake7.jpeg'),
               TitleSection(name: 'Otavalo', location: 'IMBABURA'),
-              ButtonSection(),
+              ButtonSection(mapsUrl: 'https://maps.app.goo.gl/3iXeWyyjYw66kkG67'),
               TextSection(description: 'Famoso por su mercado artesanal, Otavalo es un destino ideal para descubrir la riqueza cultural y las tradiciones indígenas del país.'),
 
               ImageSection(image: 'images/lake8.jpeg'),
               TitleSection(name: 'Quilotoa', location: 'COTOPAXI'),
-              ButtonSection(),
+              ButtonSection(mapsUrl: 'https://maps.app.goo.gl/EwWtJvAKrKdHaLdq6'),
               TextSection(description: 'La laguna del Quilotoa sorprende con sus aguas de color turquesa ubicadas en el cráter de un volcán, rodeada de impresionantes paisajes andinos.'),
 
               ImageSection(image: 'images/lake9.jpeg'),
               TitleSection(name: 'Mindo', location: 'PICHINCHA'),
-              ButtonSection(),
+              ButtonSection(mapsUrl: 'https://maps.app.goo.gl/L6oHsL9WiFD3ivoG8'),
               TextSection(description: 'Mindo es un destino natural ideal para los amantes de la flora y fauna, con una gran diversidad de especies en su entorno.'),
 
               ImageSection(image: 'images/lake10.jpeg'),
               TitleSection(name: 'Papallacta', location: 'NAPO'),
-              ButtonSection(),
+              ButtonSection(mapsUrl: 'https://maps.app.goo.gl/Ge9rKBJc6WzTfKd88'),
               TextSection(description: 'Papallacta es un lugar famoso por su clima tropical y sus paisajes exuberantes, perfecto para disfrutar de la naturaleza.'),
 
             ],
@@ -111,7 +112,17 @@ class TitleSection extends StatelessWidget {
 }
 
 class ButtonSection extends StatelessWidget {
-  const ButtonSection({super.key});
+  const ButtonSection({super.key,required this.mapsUrl,});
+
+  final String mapsUrl;
+
+  Future<void> _openMaps() async {
+    final Uri url = Uri.parse(mapsUrl);
+
+    if (!await launchUrl(url, mode: LaunchMode.externalApplication)) {
+      throw Exception('No se pudo abrir Google Maps');
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -120,9 +131,9 @@ class ButtonSection extends StatelessWidget {
       child: Row(
         mainAxisAlignment: MainAxisAlignment.spaceEvenly,
         children: [
-          ButtonWithText(color: color, icon: Icons.call, label: 'CALL'),
-          ButtonWithText(color: color, icon: Icons.near_me, label: 'ROUTE'),
-          ButtonWithText(color: color, icon: Icons.share, label: 'SHARE'),
+          ButtonWithText(color: color, icon: Icons.call, label: 'CALL',onPressed: () {},),
+          ButtonWithText(color: color, icon: Icons.near_me, label: 'ROUTE', onPressed: _openMaps,),
+          ButtonWithText(color: color, icon: Icons.share, label: 'SHARE', onPressed: () {},),
         ],
       ),
     );
@@ -136,31 +147,36 @@ class ButtonWithText extends StatelessWidget {
     required this.color,
     required this.icon,
     required this.label,
+    required this.onPressed,
   });
 
   final Color color;
   final IconData icon;
   final String label;
+  final VoidCallback onPressed;
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      mainAxisSize: MainAxisSize.min,
-      mainAxisAlignment: MainAxisAlignment.center,
-      children: [
-        Icon(icon, color: color),
-        Padding(
-          padding: const EdgeInsets.only(top: 8),
-          child: Text(
-            label,
-            style: TextStyle(
-              fontSize: 12,
-              fontWeight: FontWeight.w400,
-              color: color,
+    return InkWell(
+      onTap: onPressed,
+      child: Column(
+        mainAxisSize: MainAxisSize.min,
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Icon(icon, color: color),
+          Padding(
+            padding: const EdgeInsets.only(top: 8),
+            child: Text(
+              label,
+              style: TextStyle(
+                fontSize: 12,
+                fontWeight: FontWeight.w400,
+                color: color,
+              ),
             ),
           ),
-        ),
-      ],
+        ],
+      ),
     );
   }
 }
